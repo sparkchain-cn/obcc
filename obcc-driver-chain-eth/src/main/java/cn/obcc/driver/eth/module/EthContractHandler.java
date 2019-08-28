48ed21c2a12b46f9da4b96b0090f9561d83e0846
aiy <530365623@qq.com>
2019-08-28 16:03:50 +0800

    添加  合约项目  修改方法

--- obcc-driver-chain-eth/src/main/java/cn/obcc/driver/eth/module/EthContractHandler.java
+++ obcc-driver-chain-eth/src/main/java/cn/obcc/driver/eth/module/EthContractHandler.java
@@ -1,64 +1,18 @@
 package cn.obcc.driver.eth.module;
 
-import cn.obcc.driver.module.fn.IContractCompileFn;
-import cn.obcc.driver.module.fn.IContractDeployFn;
-import cn.obcc.driver.module.fn.IContractInvokeFn;
-import cn.obcc.driver.vo.ContractCompile;
-import cn.obcc.driver.vo.SrcAccount;
 import cn.obcc.config.ReqConfig;
+import cn.obcc.driver.contract.ContractHandler;
+import cn.obcc.driver.module.IContractHandler;
+import cn.obcc.driver.module.fn.IContractDeployFn;
+import cn.obcc.driver.vo.SrcAccount;
+import cn.obcc.vo.driver.ContractInfo;
 import org.web3j.protocol.Web3j;
 
-import cn.obcc.driver.base.BaseHandler;
-import cn.obcc.driver.module.IContractHandler;
-import cn.obcc.vo.driver.ContractInfo;
-import cn.obcc.vo.RetData;
 
-
-public class EthContractHandler extends BaseHandler<Web3j> implements IContractHandler<Web3j> {
-
+public class EthContractHandler extends ContractHandler<Web3j> implements IContractHandler<Web3j> {
 
     @Override
-    public RetData<ContractCompile> compile(String bizId, String contract, ReqConfig<Web3j> config) throws Exception {
-        return null;
-    }
+    protected void deploy(SrcAccount srcAccount, ContractInfo contractInfo, IContractDeployFn fn, ReqConfig<Web3j> config) {
 
-    @Override
-    public void compile(String bizId, String contract, IContractCompileFn fn, ReqConfig<Web3j> config) throws Exception {
-
-    }
-
-    @Override
-    public ContractInfo getContract(String bizId, String contractName) throws Exception {
-        return null;
-    }
-
-    @Override
-    public RetData<Boolean> addContract(String bizId, ContractInfo params) throws Exception {
-        return null;
-    }
-
-//    @Override
-//    public ContractInfo getContract(String bizId) throws Exception {
-//        return null;
-//    }
-
-    @Override
-    public RetData<String> deploy(String bizId, SrcAccount srcAccount, ContractInfo contract, IContractDeployFn fn, ReqConfig<Web3j> config) throws Exception {
-        return null;
-    }
-
-    @Override
-    public RetData<String> deploy(String bizId, SrcAccount srcAccount, String contract, String name, IContractDeployFn fn, ReqConfig<Web3j> config) throws Exception {
-        return null;
-    }
-
-    @Override
-    public RetData<Object> query(ContractInfo contractInfo, ReqConfig<Web3j> config, String methodName, Object... params) throws Exception {
-        return null;
-    }
-
-    @Override
-    public RetData<String> invoke(String bizId, ContractInfo contractInfo, SrcAccount srcAccount, ReqConfig<Web3j> config, IContractInvokeFn fn, Object... params) throws Exception {
-        return null;
     }
 }
